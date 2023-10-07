70b73ae739ebad5eed39ba277957c6a5834ee53c
RaySlash <stevemathewjoy@gmail.com>
2023-10-08 00:38:23 +1000

    config: clean comments and update

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,10 +1,8 @@
--- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>f', builtin.find_files, {})
 vim.keymap.set('n', '<leader>g', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>a', ':keepjumps normal! ggVG<cr>')
 vim.keymap.set('n', '<leader>w', '<cmd>write<cr>')
 vim.keymap.set('n', '<leader>q', '<cmd>q<cr>')
 vim.keymap.set({'n', 'x'}, 'x', '_x')
-vim.keymap.set('n', '<leader>a', ':keepjumps normal! ggVG<cr>')
