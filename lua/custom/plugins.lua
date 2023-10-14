e5bf38e0707bd3cf375d3d3529fd867548a2b0f6
RaySlash <stevemathewjoy@gmail.com>
2023-10-14 22:27:37 +1000

    nvchad: add tree-sitter-hypr

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -50,16 +50,6 @@ local plugins = {
   {
     "luckasRanarison/tree-sitter-hypr",
     event = "BufRead */hypr/*.conf",
-    config = function()
-      require("nvim-treesitter.parsers").get_parser_configs().hypr = {
-        install_info = {
-          url = "https://github.com/luckasRanarison/tree-sitter-hypr",
-          files = { "src/parser.c" },
-          branch = "master",
-        },
-        filetype = "hypr",
-      }
-    end,
   }
   -- To make a plugin not be loaded
   -- {
